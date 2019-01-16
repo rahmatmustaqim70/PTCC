729132250343423ac3afc066acca5fd808f9ad6c
rahmatmustaqim70 <rahmatmustaqim70@gmail.com>
2019-01-16 21:54:58 +0700

    ptcc

---
+++ README.md
@@ -0,0 +1,13 @@
+<h1>Praktik Teknologi Cloud Computing</h1>
+
+============ Isi Repository ============ </br>
+
+1. Halaman Utama = add_to_cart.html </br>
+
+2. Halaman Kedua = my_troly.html </br>
+
+Dibuat menggunakan bahasa markup yaitu html </br>
+
+Dan juga bahasa pemrograman yaitu Javascript </br>
+
+Tujuan dibuatnya untuk menyelesaikan Tugas Akhir Semester </br>
---
+++ hhr.js
@@ -0,0 +1,13 @@
+// var el = document.querySelector(".slider_menu2");
+
+// el.addEventListener("click", function(e){
+// 	console.log(e)
+// 	var img = e.target;
+// 	if(img.classList.contains('in_troly')){
+// 		console.log(img);
+// 		console.log(img.parentElement);
+// 		var parent = img.parentElement;
+
+// 		console.log(parent.children[0].textContent);
+// 	}
+// });
